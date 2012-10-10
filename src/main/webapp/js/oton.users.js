6be80dcd3fd4db3a72906695e3550410e0e41a76
Victor K <mercurievv@gmail.com>
2012-10-10 15:54:28 +0300

    Rest login, sign in thru proxy

--- src/main/webapp/js/oton.users.js
+++ src/main/webapp/js/oton.users.js
@@ -6,58 +6,52 @@
  * To change this template use File | Settings | File Templates.
  */
 function loginUser($, login, password, errorCallback) {
+    $.cookie.json = true;
     $.cookie('login', login);
     $.cookie('password', password);
-    //auth = "Basic " + window.btoa(login + ":" + password);
-    url = 'http://localhost:8484/Service-1.0/rest/user';
-/*
-    xauth.init({
-        node:document.getElementById("wrapper"),
-        url:'http://localhost:8484/Service-1.0/rest/user'
-    }).addCallback(function (status, token, node) {
-            alert([status, token, node]);
-            node.style.display = "none";
+    auth = "Basic " + window.btoa(login + ":" + password);
+    url = '/rest/user';
+    $.ajax({
+        type:'GET',
+        url: url,
+        contentType:"application/x-www-form-urlencoded",
+        xhrFields:{
+            withCredentials:true
+        },
+        accept:"application/json",
+        //contentType : "application/json",
+        cache:false,
+        cookie:false,
+        dataType : "json",
+        beforeSend:function (xhr) {
+            xhr.setRequestHeader("Authorization", auth);
+        },
+        success:function (data, textStatus) {
+            $.cookie('user', data);
+            window.location.replace("/index.html");
+        },
+        error:errorCallback
+    });
+
+
+    function signIn($, user) {
+        $.ajax({
+            data:user,
+            type:'POST',
+            url:'/rest/unchecked/user',
+            contentType:"application/json",
+            dataType:"json",
+            success:function (data, text) {
+                alert("all saved");
+                loginUser($, login, password, function (jqXHR, exception) {
+                    alert(jqXHR.status + " something wrong");
+                })
+            },
+            error:function (jqXHR, exception) {
+                if (jqXHR.status == 409)
+                    alert("User already exist")
+            }
         });
-*/
-
-    xml = new XMLHttpRequest();
-    xml.open('GET',url);
-    //xml.setRequestHeader('Authorization', auth);
-    xml.send( null );
-    return xml.responseText;
-
-
-/*
-this.http.open("get", 'http://localhost:8484/Service-1.0/rest/user', false, login, password);
-this.http.send("");
-if (http.status == 200) {
-document.location = this.action;
-} else {
-alert("Incorrect username and/or password.");
-}
-return false;
-*/
-    /*
-     $.ajax({
-     type:'GET',
-     url:'http://localhost:8484/Service-1.0/rest/user',
-     contentType:"application/x-www-form-urlencoded",
-     xhrFields : {
-     withCredentials : true
-     },
-     accept : "application/json",
-     //contentType : "application/json",
-     cache : false,
-     cookie : false,
-     beforeSend:function (xhr) {
-     xhr.setRequestHeader("Authorization", "Basic " + window.btoa(login + ":" + password)) ;
-     },
-     success:function (data, text) {
-     alert("Logged in");
-     window.location.replace("/index.html");
-     },
-     error: errorCallback
-     });
-     */
+    }
 }
 
---
+++ tests/gameIconsTest.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: MercurieVV
+ * Date: 12.9.8
+ * Time: 19:12
+ * To change this template use File | Settings | File Templates.
+ */
+GameIconsTest = TestCase("GameIconsTest");
+
+GameIconsTest.prototype.testGreet = function() {
+    gameIcons = new GameIcons();
+    //assertEquals("<table id=\"1\" class=\"\"><tr id=\"1_1\"><td id=\"1_1_1\"></td></tr></table>", gameIcons.createTable(1,1,1,""));
+};

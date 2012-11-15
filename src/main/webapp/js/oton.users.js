7c7884ad0d40efc88b45696f2a1670db70cea5cf
Victor K <mercurievv@gmail.com>
2012-11-15 18:40:57 +0200

    Login user small bugfixes

--- src/main/webapp/js/oton.users.js
+++ src/main/webapp/js/oton.users.js
@@ -5,51 +5,53 @@
  * Time: 05:30
  * To change this template use File | Settings | File Templates.
  */
-function loginUser($, login, password, errorCallback) {
-    saveAuthToCookies($, login, password);
-    authGet($, '/rest/unchecked/user?userName='+ login+'&password='+ password, errorCallback);
-}
-
-function signIn($, user) {
+function signIn($, user, error) {
     post($, '/rest/unchecked/user', user,
             function (data, text) {
-                loginUser($, data.userName, data.password);
+                loginUser($, user.userName, user.password);
             },
-            function (jqXHR, exception) {
-                if (jqXHR.status == 409)
-                    alert("User already exist")
-            }
+            error
     );
 }
 
 function saveUserSettings(user) {
     authPost($, 'rest/user', user,
-                function (data, text) {
-                    $.cookie('user', data);
-                    //saveAuthToCookies($, data.userName, data.password); password is modified on server
-                    loginCookiesUser($);
-                },
-                function (jqXHR, exception) {
-                    if (jqXHR.status == 409)
-                        alert("User already exist")
+            function (data, text) {
+                if (user.password != "") {
+                    loginUser($, user.userName, user.password);
                 }
+                window.location.replace("/index.html");
+            }
     );
 }
 
+function loginUser($, login, password, errorCallback) {
+    saveAuthToCookies($, login, password);
+    authGet($, '/rest/unchecked/user?userName=' + login + '&password=' + password,
+            function (data, textStatus) {
+                        $.cookie('user', data);
+                        window.location.replace("/index.html");
+                    }
+            ,errorCallback);
+}
+
+
 function post($, url, data, successFunc, errorFunc) {
     $.ajax({
-        data: "data = " + JSON.stringify(data),
+        data:JSON.stringify(data),
         type:'POST',
         url:url,
         contentType:"application/json",
-        dataType:"json"
+        dataType:"json",
+        success:successFunc,
+        error:errorFunc
     });
 }
 
 function authPost($, url, data, successFunc, errorFunc) {
     var auth = $.cookie('auth');
     $.ajax({
-        data: JSON.stringify(data),
+        data:JSON.stringify(data),
         type:'POST',
         url:url,
         contentType:"application/json",
@@ -57,12 +59,12 @@ function authPost($, url, data, successFunc, errorFunc) {
         beforeSend:function (xhr) {
             xhr.setRequestHeader("Authorization", "Basic " + auth);
         },
-        success: successFunc,
-        error: errorFunc
+        success:successFunc,
+        error:errorFunc
     });
 }
 
-function authGet($, url, errorCallback) {
+function authGet($, url, successCallback, errorCallback) {
     $.cookie.json = true;
     var auth = $.cookie('auth');
     $.ajax({
@@ -79,15 +81,12 @@ function authGet($, url, errorCallback) {
         beforeSend:function (xhr) {
             xhr.setRequestHeader("Authorization", "Basic " + auth);
         },
-        success:function (data, textStatus) {
-            $.cookie('user', data);
-            window.location.replace("/index.html");
-        },
+        success:successCallback,
         error:function (data, textStatus) {
             $.cookie('user', null);
             if (errorCallback != 'undefined')
                 errorCallback(data, textStatus);
-                //on permission denied window.location.replace("/login.html");
+            //on permission denied window.location.replace("/login.html");
         }
     });
 }
